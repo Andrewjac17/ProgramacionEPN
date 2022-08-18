aff4e30408a95e1bff9ea1a0fac285ce6acd3cf7
Andres <andjacmor@gmail.com>
2022-08-18 18:07:56 -0500

    Subir archivo

---
+++ exam11.py
@@ -0,0 +1,19 @@
+#+TITLE: Ejemplo pyton
+#+AUTOR: Andres Morocho
+
+#+BEGIN_SRC python :results output :exports both
+
+import numpy as np
+
+a = 5 + np.pi
+b = 6**2
+
+c  = a + b
+
+print (c)
+#+END_SRC
+
+#+RESULTS:
+
+
+
